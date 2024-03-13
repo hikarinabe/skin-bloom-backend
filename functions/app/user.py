d08cc0236a9810934f8fad3a522d2be89ac0e2e4
Namiko <namiko.trym@gmail.com>
2024-03-13 18:03:29 +0900

    fix for errors

--- functions/app/user.py
+++ functions/app/user.py
@@ -1,12 +1,15 @@
-def get_user():
+from firebase_functions import https_fn
+
+
+def get_user(req: https_fn.Request):
     print("get_user")
     return {"user_response": ""}
 
-def create_user():
+def create_user(req: https_fn.Request):
     print("create_user")
 
-def delete_user():
+def delete_user(req: https_fn.Request):
     print("GEdelete_userT")
 
-def update_user():
+def update_user(req: https_fn.Request):
     print("update_user")
