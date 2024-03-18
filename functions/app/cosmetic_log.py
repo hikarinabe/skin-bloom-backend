7d546c2555736c058be0ecce1ed48201073e439c
Namiko <namiko.trym@gmail.com>
2024-03-18 22:03:18 +0900

    implement update function

--- functions/app/cosmetic_log.py
+++ functions/app/cosmetic_log.py
@@ -1,10 +1,8 @@
 import json
 from datetime import datetime
-from zoneinfo import ZoneInfo
 
 from firebase_admin import firestore
 from firebase_functions import https_fn
-from google.api_core.datetime_helpers import DatetimeWithNanoseconds
 
 DEFAULT_BIRTHDAY = datetime(1900, 1, 1, 0, 0, 0)
 def set_timestamp_birthday(str_time):
@@ -64,10 +62,54 @@ def get_cosmetic_log(req: https_fn.Request):
     return https_fn.Response(status=200, response=json.dumps(resp), content_type='application/json')
 
 def list_cosmetic_log(req: https_fn.Request):
-    format_response(200, "unimplemented")
+    user_id = req.args.to_dict().get('user_id')
+
+    db = firestore.client()
+    log_docs = db.collection(u'user').document(user_id).collection(u'cosmetic_logs').get()
+
+    logs = []
+    
+    # TODO: cosmetic_dataから商品名も持ってくる    
+    for d in log_docs:
+        log_dict = d.to_dict()
+        one_log = {
+            'rate': log_dict['rate'],
+            'category': log_dict['category'],
+            'good_tag': log_dict['good_tag'], 
+            'bad_tag': log_dict['bad_tag'],
+            'comment': log_dict['comment'],
+        } 
+        logs.append(one_log)
+    
+    return https_fn.Response(status=200, response=json.dumps({"list_cosmetics": logs}), content_type='application/json')
+
+def validate_item(item, data, db_info):
+    if (item in data) == False:
+        return db_info.to_dict()[item]
+    return data[item]
 
 def update_cosmetic_log(req: https_fn.Request):
-    format_response(200, "unimplemented")
+    data = json.loads(req.data.decode("utf-8"))
+    user_id = data['user_id']
+    cosmetic_id = data['cosmetic_id']
+
+    db = firestore.client()
+    cosmetic_ref = db.collection(u'user').document(user_id).collection(u'cosmetic_logs').document(cosmetic_id)
+    cosmetic_info = cosmetic_ref.get()
+    if cosmetic_info.exists == False:
+        return format_response(status=404, response="log not found")
+
+    cosmetic_ref.update({
+        'rate': validate_item('rate', data, cosmetic_info),
+        'category': validate_item('category', data, cosmetic_info),
+        'good_tag': validate_item('good_tag', data, cosmetic_info), 
+        'bad_tag': validate_item('bad_tag', data, cosmetic_info), 
+        'comment': validate_item('comment', data, cosmetic_info),
+        'update_time': datetime.now()
+    })
+
+    return format_response(status=201, response='Success to update log')
+    
 
 def delete_cosmetic_log(req: https_fn.Request):
     user_id = req.args.to_dict().get('user_id')
