f8b1d13ac9fe439b009489d96add6cd5a86d5658
Namiko <namiko.trym@gmail.com>
2024-03-19 14:16:33 +0900

    getとseachの実装

---
+++ functions/app/cosmetic_info.py
@@ -0,0 +1,61 @@
+import json
+from datetime import datetime
+
+from firebase_admin import firestore
+from firebase_functions import https_fn
+
+
+def format_response(status, response: str):
+    return https_fn.Response(status=status, response=json.dumps({'message': response}), content_type='application/json')
+
+def get_cosmetic_info(req: https_fn.Request):
+    cosmetic_id = req.args.to_dict().get('cosmetic_id')
+    if cosmetic_id == None:
+        return format_response(status=400, response="please specify cosmetic_id")
+    
+    db = firestore.client()
+    doc = db.collection(u'cosmetic_data').document(cosmetic_id).get()    
+    if doc.exists == False:
+        return format_response(status=404, response="cosmetic not found")
+        
+    info_dict = doc.to_dict()
+    ingredients = info_dict['raw_ingredients'].split(',')
+    resp = {
+        'id': cosmetic_id,
+        'ingredients': ingredients,
+        'name': info_dict['category'],
+        'price': info_dict['price'], 
+        'company': info_dict['company'],
+        'category': info_dict['category'],
+    } 
+    
+    return https_fn.Response(status=200, response=json.dumps(resp), content_type='application/json')
+    
+def search_cosmetic_info(req: https_fn.Request):
+    req_data = json.loads(req.data.decode("utf-8"))    
+    db = firestore.client()
+    doc_ref = db.collection(u'cosmetic_data')
+
+    company = req_data['company'] if 'company' in req_data else None
+    if company != None:
+        doc_ref = doc_ref.where('company', '==', company)
+
+    category = req_data['category'] if 'category' in req_data else None
+    if category != None:
+        doc_ref = doc_ref.where('category', '==', category)
+    
+    doc = doc_ref.get()
+    resp = []
+    for d in doc:
+        info_dict = d.to_dict()
+        ingredients = info_dict['raw_ingredients'].split(',')
+        resp.append({
+            'id': d.id,
+            'ingredients': ingredients,
+            'name': info_dict['category'],
+            'price': info_dict['price'], 
+            'company': info_dict['company'],
+            'category': info_dict['category'],
+        })
+
+    return https_fn.Response(status=200, response=json.dumps(resp), content_type='application/json')
