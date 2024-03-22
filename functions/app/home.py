053233f492598e9fb04861cad751e6e1f79cbf8c
Namiko <namiko.trym@gmail.com>
2024-03-23 00:00:52 +0900

    add recommend service

--- functions/app/home.py
+++ functions/app/home.py
@@ -41,33 +41,50 @@ def get_mypage(req: https_fn.Request):
             'date': set_str_day(log_dict['update_time'])
         } 
         logs.append(one_log)
+
+    
     
 
     ###### レコメンド関連
     ## あとでAPI呼ぶ
-    # レコメンドのおすすめ商品
-    recommend_items = [
+    if len(logs) == 0:
+        recommend_items = recommend_0["recommend_items"]
+        good_ingredient = recommend_0["good_ingredient"]
+        bad_ingredient = recommend_0["bad_ingredient"]
+    elif len(logs) == 1:
+        recommend_items = recommend_1["recommend_items"]
+        good_ingredient = recommend_1["good_ingredient"]
+        bad_ingredient = recommend_1["bad_ingredient"]
+    else:
+        recommend_items = recommend_2["recommend_items"]
+        good_ingredient = recommend_2["good_ingredient"]
+        bad_ingredient = recommend_2["bad_ingredient"]
+ 
+      
+    
+    
+    return https_fn.Response(status=200, response=json.dumps({
+        'list_cosmetics': logs,
+        'recommend_items': recommend_items,
+        'good_ingredient': good_ingredient,
+        'bad_ingredient': bad_ingredient
+    }), content_type='application/json')
+
+
+
+recommend_0 = {
+     'recommend_items': [
         {
             "id": "01DKCIR93KatptXG3sno",
             "ingredients": [
                 "ジメチコン",
                 "グリセリン",
-                "PG",
-                "ミネラルオイル",
-                "（ジメチコン／ビニルジメチコン）クロスポリマー",
-                "（ラウリルジメチコン／ポリグリセリン-3）クロスポリマー",
-                "水",
-                "ハイドロキノン",
-                "トコフェロール",
-                "ツボクサエキス",
-                "アスコルビン酸",
-                "水添ポリイソブテン",
-                "香料"
             ],
             "name": "エピステーム HQレーザークリア",
             "price": 7480,
             "company": 130,
-            "category": 3
+            "category": 3,
+            "match_rate": 70,
         }, 
         {
             "id": "NwCyFwB7IOkRh3cCjS8T",
@@ -75,31 +92,12 @@ def get_mypage(req: https_fn.Request):
                 "グリセリン",
                 "ＤＰＧ",
                 "ＢＧ",
-                "ヒアルロン酸Ｎａ",
-                "トレハロース",
-                "油性エモリエント成分",
-                "ラウロイルグルタミン酸ジ（フィトステリル／オクチルドデシル）",
-                "トリエチルヘキサノイン",
-                "乳化剤",
-                "トリイソステアリン酸ＰＥＧ－５０水添ヒマシ油",
-                "セテス－２０",
-                "可溶化剤",
-                "ＰＥＧ－６０水添ヒマシ油",
-                "防腐剤",
-                "メチルパラベン",
-                "プロピルパラベン",
-                "ｐＨ調整剤",
-                "リンゴ酸Ｎａ",
-                "リンゴ酸",
-                "成分の酸化防止剤",
-                "トコフェロール",
-                "基剤",
-                "水"
             ],
             "name": "保湿化粧水 とてもしっとりタイプ",
             "price": 770,
             "company": 120,
-            "category": 1
+            "category": 1,
+            "match_rate": 68,
         }, 
         {
             "id": "EBvc3Z5oyXTofirGsNdF",
@@ -110,27 +108,165 @@ def get_mypage(req: https_fn.Request):
             "name": "肌ラボ 極潤 ハリパーフェクトゲル",
             "price": 1309,
             "company": 130,
-            "category": 7
+            "category": 7,
+            "match_rate": 56
         }, 
-    ]
-
+    ],
     # レコメンドの合う成分
-    good_ingredient = [
+    'good_ingredient' : [
         "ナイアシンアミド",
         "ツボクサエキス" ,
         "ヒアルロン酸Ｎａ",
-   ]
-
+   ],
     # レコメンドの合わない成分
-    bad_ingredient = [
+    'bad_ingredient' : [
         "カルボキシビニルポリマー",
         "トコフェロール (天然ビタミンE)",
         "ヤシ油脂肪酸",
     ]
-    
-    return https_fn.Response(status=200, response=json.dumps({
-        'list_cosmetics': logs,
-        'recommend_items': recommend_items,
-        'good_ingredient': good_ingredient,
-        'bad_ingredient': bad_ingredient
-    }), content_type='application/json')
+}
+     
+
+recommend_1 = {
+     'recommend_items': [
+        {
+            "id": "EBvc3Z5oyXTofirGsNdF",
+            "ingredients": [
+                "ナイアシンアミド",
+                "グリチルリチン酸2K"
+            ],
+            "name": "肌ラボ 極潤 ハリパーフェクトゲル",
+            "price": 1309,
+            "company": 130,
+            "category": 7,
+            "match_rate": 78
+        }, 
+        {
+            "id": "VvDaLpOoAzEZtX35hacY",
+            "ingredients": [
+                "カンゾウ根エキス",
+                "サクラ葉エキス",
+                "イチョウ葉エキス",
+                "ワイルドタイムエキス",
+                "オリーブ葉エキス",
+                "ワレモコウエキス",
+                "テンチャエキス",
+                "ヒアルロン酸Ｎａ",
+            ],
+            "name": "3D　ビューティーリフティング　セラム",
+            "price": 14300,
+            "company": 110,
+            "category": 3,
+            "match_rate": 78
+        }, 
+       {
+        "id": "BNUUzfw5bSAU4qrSXN4u",
+        "ingredients": [
+            "水",
+            "グリセリン",
+            "BG"
+        ],
+        "name": "エピステーム ステムサイエンスリフトクリームb",
+        "price": None,
+        "company": 130,
+        "category": 6,
+        "match_rate": 65,
+    }
+            
+    ],
+    # レコメンドの合う成分
+    'good_ingredient' : [
+        "ナイアシンアミド",
+        "ヒアルロン酸Ｎａ",
+        "カンゾウ根エキス",
+   ],
+    # レコメンドの合わない成分
+    'bad_ingredient' : [
+        "トコフェロール (天然ビタミンE)",
+        "カルボキシビニルポリマー",
+        "ヤシ油脂肪酸",
+    ]     
+}
+
+recommend_2 = {
+     'recommend_items': [
+        {
+            "id": "JBHItuSYUgFp9wT6xrhm",
+            "ingredients": [
+                "水",
+                "グリセリン",
+                "トリ（カプリル酸／カプリン酸）グリセリル",
+                "DPG",
+                "オレフィンオリゴマー",
+                "ペンチレングリコール",
+                "PPG-10メチルグルコース",
+                "ヒアルロン酸Na",
+                "加水分解ヒアルロン酸",
+                "アセチルヒアルロン酸Na",
+                "乳酸球菌／ヒアルロン酸発酵液",
+                "BG",
+                "イソステアリン酸PEG-20ソルビタン",
+                "PEG-32",
+                "PEG-6",
+                "ステアリン酸グリセリル",
+                "ジメチコン",
+                "カルボマー",
+                "プロパンジオール",
+                "ラウロイルグルタミン酸ジ（フィトステリル／オクチルドデシル）",
+                "カプリルヒドロキサム酸",
+                "TEA",
+                "EDTA-2Na",
+                "ステアリルアルコール",
+                "ベヘニルアルコール"
+            ],
+            "name": "肌ラボ 極潤 ヒアルロン乳液",
+            "price": null,
+            "company": 130,
+            "category": 2,
+            "match_rate": 85
+        },
+        {
+            "id": "EBvc3Z5oyXTofirGsNdF",
+            "ingredients": [
+                "ナイアシンアミド",
+                "グリチルリチン酸2K"
+            ],
+            "name": "肌ラボ 極潤 ハリパーフェクトゲル",
+            "price": 1309,
+            "company": 130,
+            "category": 7,
+            "match_rate": 80
+        }, 
+        {
+            "id": "VvDaLpOoAzEZtX35hacY",
+            "ingredients": [
+                "カンゾウ根エキス",
+                "サクラ葉エキス",
+                "イチョウ葉エキス",
+                "ワイルドタイムエキス",
+                "オリーブ葉エキス",
+                "ワレモコウエキス",
+                "テンチャエキス",
+                "ヒアルロン酸Ｎａ",
+            ],
+            "name": "3D　ビューティーリフティング　セラム",
+            "price": 14300,
+            "company": 110,
+            "category": 3,
+            "match_rate": 72
+        },  
+    ],
+    # レコメンドの合う成分
+    'good_ingredient' : [
+        "ヒアルロン酸Ｎａ",
+        "ナイアシンアミド",
+        "カンゾウ根エキス",
+   ],
+    # レコメンドの合わない成分
+    'bad_ingredient' : [
+        "オリーブ果実油"
+        "トコフェロール (天然ビタミンE)",
+        "カルボキシビニルポリマー",
+    ]     
+     
+}   
