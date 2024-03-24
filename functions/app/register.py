b3fae216ee1379b141fd52b74d23bbc0f61b4b31
Gre3nT <fabulous648@gmail.com>
2024-03-25 00:53:59 +0900

    add validation

--- functions/app/register.py
+++ functions/app/register.py
@@ -24,20 +24,25 @@ def register_user(req: https_fn.Request):
 
     # email
     email = req.form.get('email')
-
-    # hash password    
-    password = hashlib.sha256(req.form.get('password').encode()).hexdigest()
-
+    #validation
     db = firestore.client()
-    result = db.collection('auth').document(user_id).set({
-        'email': email, 
-        'password': password,
-        'create_time': datetime.now(),
-        'update_time': datetime.now()
-    })
-    if result:
-        return https_fn.Response(status=200, response=json.dumps(LoginResponse(user_id).__dict__), content_type='application/json')
-    return format_response(status=500, response="Failed to register user")
+    validator = db.collection('auth').where("email", "==", email).get()
+    if len(validator):
+        # hash password    
+        password = hashlib.sha256(req.form.get('password').encode()).hexdigest()
+
+        # db = firestore.client()
+        result = db.collection('auth').document(user_id).set({
+            'email': email, 
+            'password': password,
+            'create_time': datetime.now(),
+            'update_time': datetime.now()
+        })
+        if result:
+            return https_fn.Response(status=200, response=json.dumps(LoginResponse(user_id).__dict__), content_type='application/json')
+        return format_response(status=500, response="Failed to register user")
+    else:
+        return format_response(status=400,response="Bad Request")
 
 # NOTE: password check is done by client side
 def update_password(req: https_fn.Request):
