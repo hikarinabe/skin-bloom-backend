83458bcf664fb4aa0b265f4056ed2c3ca9dc9dd6
Namiko <namiko.trym@gmail.com>
2024-03-26 19:28:03 +0900

    list_cosmetic_logにorder_byを追加

--- functions/app/cosmetic_log.py
+++ functions/app/cosmetic_log.py
@@ -51,7 +51,7 @@ def get_cosmetic_log(req: https_fn.Request):
     cosmetic_id = req.args.to_dict().get('cosmetic_id')
 
     db = firestore.client()
-    doc = db.collection(u'user').document(user_id).collection(u'cosmetic_logs').document(cosmetic_id).order_by('create_time', direction="DESCENDING").get()    
+    doc = db.collection(u'user').document(user_id).collection(u'cosmetic_logs').document(cosmetic_id).get()    
     if doc.exists == False:
         return format_response(status=404, response="cosmetic not found")
     log_dict = doc.to_dict()
@@ -73,7 +73,7 @@ def list_cosmetic_log(req: https_fn.Request):
     user_id = req.args.to_dict().get('user_id')
 
     db = firestore.client()
-    log_docs = db.collection(u'user').document(user_id).collection(u'cosmetic_logs').limit(50).get()
+    log_docs = db.collection(u'user').document(user_id).collection(u'cosmetic_logs').order_by('create_time', direction="DESCENDING").limit(50).get()
 
     logs = []
     
